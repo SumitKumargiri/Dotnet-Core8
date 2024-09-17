f0e540cadbb283d1c7cd56014e8eb5d0ba23cf00
Sumitgiri15434 <96234273+SumitKumargiri@users.noreply.github.com>
2024-09-17 12:58:52 +0530

    dotnet core8 backend code

---
+++ Signalr-redis/SignalRwithmysql/SignalRwithmysql/SignalRwithmysql/Model/MessageData.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalRwithmysql.Model
+{
+    public class MessageData
+    {
+        public string Username { get; set; }
+        public string Message { get; set; }
+        public string ConnectionId { get; set; }
+    }
+}
---
+++ Signalr-redis/SignalRwithmysql/SignalRwithmysql/SignalRwithmysql/Program.cs
@@ -0,0 +1,51 @@
+using crudoperation.utility;
+using SignalRwithmysql;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Corepolicy", builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .AllowCredentials();
+    });
+});
+builder.Services.AddControllers();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("RedisConnection");
+});
+builder.Services.AddSignalR();
+
+//+++++++++++ Add DBGateway as a service +++++++++++++
+builder.Services.AddSingleton(new DBGateway(builder.Configuration.GetConnectionString("ConnectionString1")));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseCors("Corepolicy");
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapHub<ChatHub>("/ChatHub");
+});
+
+app.Run();

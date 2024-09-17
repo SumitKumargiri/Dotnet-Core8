60ac4a61a6666ab80f58da58a5b2b826839d7516
Sumitgiri15434 <96234273+SumitKumargiri@users.noreply.github.com>
2024-09-17 13:02:15 +0530

    Signalr-frontened angular17

---
+++ Signalr-redis/SignalRwithmysql/Signalr-redis-frontened/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from './service/chat.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  public username: string = '';
+  public message: string = '';
+  public connectionid: string = '';
+  public messages: { username: string, message: string, connectionid: string }[] = [];
+
+  constructor(private chatService: ChatService) {}
+
+  ngOnInit(): void {
+    this.chatService.startConnection();
+
+    this.chatService.addMessageListener((username: string, message: string, connectionid: string) => {
+      console.log(`Message received from ${username} with connectionId ${connectionid}`);
+      this.messages.push({ username, message, connectionid });
+    });
+  }
+
+  public sendMessage(): void {
+    if (this.username && this.message) {
+      this.chatService.sendMessage(this.username, this.message);
+      this.message = '';  
+    } else {
+      console.error("Username or message is empty");
+    }
+  }
+}

8f4fe045dadf582089e592d29e9b1e19a56d7f93
Sumitgiri15434 <96234273+SumitKumargiri@users.noreply.github.com>
2024-09-17 10:28:13 +0530

    SignalR Angular18

---
+++ SignalRConcept/SignalR-Frontened/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ChatService } from './service/chat.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [ChatService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ SignalRConcept/SignalR-Frontened/src/app/service/chat.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import * as signalR from '@microsoft/signalr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+  private hubConnection!: signalR.HubConnection;
+
+  constructor() { }
+
+  public startConnection(): void {
+    this.hubConnection = new signalR.HubConnectionBuilder()
+      .withUrl('https://localhost:7221/ChatHub')  
+      .build();
+
+    this.hubConnection
+      .start()
+      .then(() => console.log('Connection started'))
+      .catch(err => console.error('Error while starting connection: ', err));
+  }
+
+  public sendMessage(username: string, message: string): void {
+    this.hubConnection.invoke('SendMessage', username, message)
+      .catch(err => console.error('Error while sending message: ', err));
+  }
+
+  public addMessageListener(callback: (username: string, message: string, connectionid: string) => void): void {
+    this.hubConnection.on('ReceiveMessage', (username, message, connectionid) => {
+      callback(username, message, connectionid);
+    });
+  }
+}

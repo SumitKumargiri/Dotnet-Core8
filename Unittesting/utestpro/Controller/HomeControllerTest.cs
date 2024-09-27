a7292f63c4529029f1fa4ff4fccba4d9763a4fa1
Sumitgiri15434 <96234273+SumitKumargiri@users.noreply.github.com>
2024-09-27 15:19:46 +0530

    Basic Unit Testing

---
+++ Unittesting/utestpro/Controller/HomeControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unittesting.Controllers;
+
+namespace utestpro.Controller
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void HomeController_Index_ValidResult()
+        {
+            HomeController controller = new HomeController();
+            string expectedResult = "I am in home";
+
+            string result = controller.Index();
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}

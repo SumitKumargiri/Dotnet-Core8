f2450577bb2e29260d39b8ff4fae4bd288d5d30a
Sumitgiri15434 <96234273+SumitKumargiri@users.noreply.github.com>
2024-09-27 15:17:02 +0530

    Basic Unit Testing

---
+++ Unittesting/Unittesting/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Unittesting.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("api/[controller]")]
+        public string Index()
+        {
+            return "I am in home";
+        }
+
+      
+    }
+}
